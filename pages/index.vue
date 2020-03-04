d04742800f5ef341b06ddb39be1677852142e74e
Derek Gilbert <45082882+DerekAlanGilbert@users.noreply.github.com>
2020-03-03 19:51:06 -0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{photos}}</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  async asyncData({ $http }) {
+    const { data } = await axios.get(
+      "https://api-homie-dev.azurewebsites.net/api/v1/listings/313226"
+    );
+    return { listing: data };
+  },
+  computed: {
+    photos() {
+      return this.listing.listingPhotos;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
